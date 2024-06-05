c79c07c210c681008fea1c658c49e523313d9eb3
hiroyuki hosoda <158367633+Hiroyukihosoda@users.noreply.github.com>
2024-06-06 06:50:54 +0900

    エラー文表示機能実装

--- src/resources/views/register.blade.php
+++ src/resources/views/register.blade.php
@@ -30,7 +30,12 @@
                 <div class="form__group">
                     <div class="form__group-content">
                         <div class="form__input--text">
-                        <input type="text" name="name" placeholder="名前" />
+                        <input type="text" name="name" placeholder="名前" value="{{ old('name') }}" />
+                        </div>
+                        <div class="form__error">
+                            @error('name')
+                            {{ $message }}
+                            @enderror
                         </div>
                     </div>
                 </div>
@@ -38,7 +43,12 @@
                 <div class="form__group">
                     <div class="form__group-content">
                         <div class="form__input--text">
-                        <input type="email" name="email" placeholder="メールアドレス" />
+                        <input type="email" name="email" placeholder="メールアドレス" value="{{ old('email') }}" />
+                        </div>
+                        <div class="form__error">
+                            @error('email')
+                            {{ $message }}
+                            @enderror
                         </div>
                     </div>
                 </div>
@@ -46,7 +56,12 @@
                 <div class="form__group">
                     <div class="form__group-content">
                         <div class="form__input--text">
-                        <input type="password" name="password" placeholder="パスワード" />
+                        <input type="password" name="password" placeholder="パスワード" value="{{ old('password') }}" />
+                        </div>
+                        <div class="form__error">
+                            @error('password')
+                            {{ $message }}
+                            @enderror
                         </div>
                     </div>
                 </div>
@@ -54,7 +69,12 @@
                 <div class="form__group">
                     <div class="form__group-content">
                         <div class="form__input--text">
-                        <input type="password" name="password" placeholder="確認用パスワード" />
+                        <input type="password" name="password" placeholder="確認用パスワード" value="{{ old('password') }}" />
+                        </div>
+                        <div class="form__error">
+                            @error('password')
+                            {{ $message }}
+                            @enderror
                         </div>
                     </div>
                 </div>
